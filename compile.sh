f252115adee07e06d6920348797ff04a3bba5141
zehua mai <mai.zehua@gmail.com>
2012-05-13 13:34:33 -0400

    add bound to instanes, prevent long time processing

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+javac -d bin -cp stanford-parser.jar:guava-12.0.jar src/edu/nyu/cs/final_project/*.java
+
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+java -cp stanford-parser.jar:guava-12.0.jar:bin edu.nyu.cs.final_project.Main $1
